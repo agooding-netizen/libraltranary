a774b88ca4659cd3684605341a5b30a7bfec03d7
lewisfisher00 <lfisher@altranuk.local>
2020-12-07 10:45:33 +0000

    start to change login

--- Library Flask.py
+++ Library Flask.py
@@ -35,10 +35,6 @@ class User(UserMixin):
         return "%d/%s/%s" % (self.id, self.name, self.password)
 
 
-# create some users with ids 1 to 20
-users = [User(id) for id in range(1, 21)]
-
-
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -117,7 +113,8 @@ def upload_files(title, author, status, quantity):
 
     update_book(title=title, author=author, image=filename)
 
-    return redirect(url_for('book_with_cover', title=title, author=author, status=status, quantity=quantity, image=uploaded_file.filename))
+    return redirect(url_for('book_with_cover', title=title, author=author, status=status, quantity=quantity,
+                            image=uploaded_file.filename))
 
 
 @app.route('/upload-image-<title>-<author>-<status>-<quantity>-<image>')
@@ -214,7 +211,25 @@ def login():
             username = request.form['librarian_id']
             password = request.form['librarian_pwd']
 
-        if password == username + "_secret":
+        find_user = """ SELECT member_name, password FROM members WHERE member_name = ?; """
+
+        database = get_db()
+        cursor = database.cursor()
+        cursor.execute(find_user, (username,))
+        data = cursor.fetchone()
+
+        if data is None:
+            return abort(401)
+        elif password == data[1]:
+            id = username.split('user')[1]
+            user = User(id)
+            login_user(user)
+            if not librarian:
+                return redirect("user-login")
+            else:
+                return redirect("librarian-login")
+
+        '''if password == username + "_secret":
             id = username.split('user')[1]
             user = User(id)
             login_user(user)
@@ -223,7 +238,7 @@ def login():
             else:
                 return redirect("librarian-login")
         else:
-            return abort(401)
+            return abort(401)'''
     else:
         return render_template('Login.html')
 
